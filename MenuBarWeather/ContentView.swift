9cc4576b96dd277355ed98338c9b529059f7853c
gnehs <gnehs@gnehs.net>
2024-06-15 16:48:44 +0800

    Initial Commit

---
+++ MenuBarWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MenuBarWeather
+//
+//  Created by 勝勝寶寶 on 2024/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MenuBarWeather/MenuBarWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarWeatherApp.swift
+//  MenuBarWeather
+//
+//  Created by 勝勝寶寶 on 2024/6/15.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
