de29fffe46907c29d7b89b0066e6f134bb8fc29e
gnehs <gnehs@gnehs.net>
2024-06-16 14:21:38 +0800

    updated userscript

--- MenuBarWeather/ContentView.swift
+++ MenuBarWeather/ContentView.swift
@@ -13,11 +13,14 @@ struct ContentView: View {
         VStack{
             HStack{
                 Text("Menu Bar Weather")
+                    .fontWeight(.bold)
                 Spacer()
-                Button("結束") {
+                Button("Quit") {
                     NSApplication.shared.terminate(nil)
                 }
-            }  .padding(.horizontal, 16.0).padding(.top, 8.0)
+            }
+            .padding([.top, .trailing], 8.0)
+            .padding(.leading, 16.0)
             WebView(urlString: "https://www.google.com.tw/search?q=weather", configuration: configuration)
             { webView in
                 webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
@@ -31,21 +34,42 @@ struct ContentView: View {
     // Example of WKWebViewConfiguration
     var configuration: WKWebViewConfiguration {
         let userScriptString = """
- const {x, y} = document.querySelector('#wob_wc').getClientRects()[0];
- document.body.style.transform = `translate(-${x}px, -${y}px)`;
- document.body.style.overflow = 'hidden';
+  const ob = new MutationObserver(function (mutationsList, observer) {
+    injectScript();
+  });
+  ob.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+ function injectScript(){
+    const weatherContainer = document.querySelector('#wob_wc');
+    if (weatherContainer) {
+        if (!weatherContainer.hasAttribute('data-injected')) {
+            weatherContainer.setAttribute('data-injected', 'true');
+            weatherContainer.style.position = 'fixed';
+            weatherContainer.style.top = '0';
+            weatherContainer.style.left = '0';
+            weatherContainer.style.zIndex = '99999';
+            weatherContainer.style.backgroundColor = '#fff';
+            document.body.style.overflow = 'hidden';
+            document.body.style.opacity = '1';
+        }
+    } else {
+        document.body.style.opacity = '0';
+    }
+ }
 """
         let  userScript = WKUserScript(
             source: userScriptString,
-            injectionTime: .atDocumentEnd,
+            injectionTime: .atDocumentStart,
             forMainFrameOnly: true)
-        
+
         let userContentController = WKUserContentController()
         userContentController.addUserScript(userScript)
-        
+
         let configuration = WKWebViewConfiguration()
         configuration.userContentController = userContentController
-        
+
         return configuration
     }
 }
